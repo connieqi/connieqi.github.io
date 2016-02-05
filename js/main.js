cf0a192a21316543896a2f283f746a889edd7d48
Connie Qi <connieqi@umich.edu>
2016-02-05 00:54:43 -0800

    js style

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-    // Cache selectors for faster performance.
-    var $window = $(window),
-    	$header = $('#header'),
-        $name = $('#name'),
-        $nameAnchor = $('#name-anchor'),
-        $faceIcon = $('#face-icon');
+  // Cache selectors for faster performance.
+  var $window = $(window);
+  var $header = $('#header');
+  var $name = $('#name');
+  var $nameAnchor = $('#name-anchor');
+  var $faceIcon = $('#face-icon');
 
-    // $header.height($window.height());
+  // $header.height($window.height());
 
-    // Run this on scroll events.
-    $window.scroll(function() {
-        var window_top = $window.scrollTop();
-        var name_top = $nameAnchor.offset().top;
-        var face_top = $faceIcon.offset().top;
+  // Run this on scroll events.
+  $window.scroll(function() {
+    var window_top = $window.scrollTop();
+    var name_top = $nameAnchor.offset().top;
+    var face_top = $faceIcon.offset().top;
 
-        if (window_top > name_top) {
-            // Make the div sticky.
-            $name.addClass('sticky');
-            $nameAnchor.height($name.height());
-        }
-        else {
-            // Unstick the div.
-            $name.removeClass('sticky');
-            $nameAnchor.height(0);
-        }
+    if (window_top > name_top) {
+      // Make the div sticky.
+      $name.addClass('sticky');
+      $nameAnchor.height($name.height());
+    }
+    else {
+      // Unstick the div.
+      $name.removeClass('sticky');
+      $nameAnchor.height(0);
+    }
 
-        // fade out face icon
-        $faceIcon.css({
-            'opacity': ((face_top - window_top) / face_top)
-        });
+    // fade out face icon
+    $faceIcon.css({
+      'opacity': ((face_top - window_top) / face_top)
     });
+  });
 });
